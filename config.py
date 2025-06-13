7b96937ef744e170475424d6eea528bc818e6889
Arif Mustafa Khan <98647115+ArifMustafaKhan@users.noreply.github.com>
2025-06-13 18:15:58 +0530

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_id = "28485705"
 api_hash = "26d76803d487d6d0cc818cf191fa7518"
 bot_token = "8029450146:AAHUo-Q9dT5UE12lZWhgZGUWQyyDlQJj7WQ"
-auth_users = [6211690957]
-sudo_users = [6211690957]
+auth_users = [6126740740]
+sudo_users = [6126740740]
